4d3c8293308dada374e6da1d0b464cb030a67db3
Luis Sotomayor <luis.sotomayor@gmail.com>
2014-12-02 23:59:06 -0800

    Added helpers and first channel factory.

---
+++ WcfCodeConfiguration.Helpers/ChannelFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace WcfCodeConfiguration.Helpers
+{
+    public class ChannelFactory<T>
+    {
+        private System.ServiceModel.ChannelFactory<T> _channelFactory;
+
+        public ChannelFactory()
+        {
+            _channelFactory = new System.ServiceModel.ChannelFactory<T>(new NetTcpBinding());
+        }
+    }
+}
