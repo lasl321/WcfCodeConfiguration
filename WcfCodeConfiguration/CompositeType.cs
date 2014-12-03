beacf797a4e82772b9427f099744f9e1fab25281
Luis Sotomayor <luis.sotomayor@gmail.com>
2014-12-02 21:53:36 -0800

    Renamed service.

---
+++ WcfCodeConfiguration/CompositeType.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace WcfCodeConfiguration
+{
+    [DataContract]
+    public class CompositeType
+    {
+        public CompositeType()
+        {
+            StringValue = "Hello ";
+            BoolValue = true;
+        }
+
+        [DataMember]
+        public bool BoolValue { get; set; }
+
+        [DataMember]
+        public string StringValue { get; set; }
+    }
+}
---
+++ WcfCodeConfiguration/IEchoService.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace WcfCodeConfiguration
+{
+    [ServiceContract]
+    public interface IEchoService
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+    }
+}
