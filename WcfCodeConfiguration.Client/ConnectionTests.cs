a964fb8bfb19a52d31318402a13e76e15bfd52a5
Luis Sotomayor <luis.sotomayor@gmail.com>
2014-12-03 22:28:06 -0800

    Added first version of code configuration.

---
+++ WcfCodeConfiguration.Client/ConnectionTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Security;
+using Microsoft.WindowsAzure;
+using NUnit.Framework;
+using WcfCodeConfiguration.Contract;
+
+namespace WcfCodeConfiguration.Client
+{
+    public class ConnectionTests
+    {
+        private string _host;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _host = CloudConfigurationManager.GetSetting("Host");
+        }
+
+        [Test]
+        public void ShouldConnectToServer()
+        {
+            var service = GetInstance(_host);
+            var data = service.GetData(1111);
+
+            Assert.That(data, Is.EqualTo("You entered: 1111"));
+        }
+
+        [Test]
+        public void ShouldConnectToServer2()
+        {
+            var service = GetInstance(_host);
+            var data = service.GetDataUsingDataContract(new CompositeType
+            {
+                BoolValue = true,
+                StringValue = "FooBar"
+            });
+
+            Assert.That(data,
+                        Is.EqualTo(new CompositeType
+                        {
+                            BoolValue = true,
+                            StringValue = "FooBarSuffix"
+                        }));
+        }
+
+        private static IEchoService GetInstance(string host)
+        {
+            var uri = new Uri(string.Format("net.tcp://{0}:50001/EchoService.svc", host));
+            var address = new EndpointAddress(uri, new DnsEndpointIdentity("localhost"));
+            var binding = new NetTcpBinding(SecurityMode.Transport)
+            {
+                Security =
+                {
+                    Transport = {ClientCredentialType = TcpClientCredentialType.None}
+                }
+            };
+            var factory = new ChannelFactory<IEchoService>(binding, address);
+            var endpointBehavior = factory.Endpoint.EndpointBehaviors[typeof (ClientCredentials)];
+            var clientCredentials = (ClientCredentials) endpointBehavior;
+            clientCredentials.ServiceCertificate.Authentication.CertificateValidationMode =
+                X509CertificateValidationMode.None;
+
+            return factory.CreateChannel();
+        }
+    }
+}
--- WcfCodeConfiguration.Contract/CompositeType.cs
+++ WcfCodeConfiguration.Contract/CompositeType.cs
@@ -5,16 +5,35 @@ namespace WcfCodeConfiguration.Contract
     [DataContract]
     public class CompositeType
     {
-        public CompositeType()
-        {
-            StringValue = "Hello ";
-            BoolValue = true;
-        }
-
         [DataMember]
         public bool BoolValue { get; set; }
 
         [DataMember]
         public string StringValue { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("BoolValue: {0}, StringValue: {1}", BoolValue, StringValue);
+        }
+
+        protected bool Equals(CompositeType other)
+        {
+            return BoolValue.Equals(other.BoolValue) && string.Equals(StringValue, other.StringValue);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((CompositeType) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (BoolValue.GetHashCode() * 397) ^ (StringValue != null ? StringValue.GetHashCode() : 0);
+            }
+        }
     }
 }
